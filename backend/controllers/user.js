00a68755af7518715364b03aa1f402084fae0a4d
Romain Descamps <descampsr@hotmail.fr>
2021-10-11 14:19:05 +0200

    install package sécu/valid data

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,2 +1,4 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const cryptojs = require('crypto-js');
+const Cookies = require('cookies');
---
+++ backend/utils/database.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql12');
+
+exports.connect = () => {
+    const connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: '3306',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_BASENAME,
+        charset: 'utf8_general_ci'
+    });
+    return connection;
+}
+
+exports.pool = () => {
+    const pool = mysql.createPool({
+      host     : process.env.DB_HOST,
+      user     : process.env.DB_USER,
+      password : process.env.DB_PASSWORD,
+      database : process.env.DB_BASENAME,
+      charset: 'utf8_general_ci',
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0
+    });
+    return pool
+}
